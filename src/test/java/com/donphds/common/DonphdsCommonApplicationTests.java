0a012ed5e7e354ec68172d5477413595ad51631f
tangbo <tangbo@lixiang.com>
2022-09-29 10:15:22 +0800

    initial: project

---
+++ src/test/java/com/donphds/common/DonphdsCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donphds.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonphdsCommonApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
